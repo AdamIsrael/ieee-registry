aa637b006261fd0ef5c3f28b44cfbda55dcc5179
Adam Israel <adam@adamisrael.com>
2023-12-30 20:46:13 -0500

    fmt

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,36 @@ use ieee_registry::*;
 
 fn main() {
     println!("Caching IEEE registry file(s)...");
-    println!("✔ {}", expanduser(get_cid_path().unwrap()).unwrap().display());
-    println!("✔ {}", expanduser(get_eth_path().unwrap()).unwrap().display());
-    println!("✔ {}", expanduser(get_iab_path().unwrap()).unwrap().display());
-    println!("✔ {}", expanduser(get_mam_path().unwrap()).unwrap().display());
-    println!("✔ {}", expanduser(get_manid_path().unwrap()).unwrap().display());
-    println!("✔ {}", expanduser(get_opid_path().unwrap()).unwrap().display());
-    println!("✔ {}", expanduser(get_oui_path().unwrap()).unwrap().display());
-    println!("✔ {}", expanduser(get_oui36_path().unwrap()).unwrap().display());
+    println!(
+        "✔ {}",
+        expanduser(get_cid_path().unwrap()).unwrap().display()
+    );
+    println!(
+        "✔ {}",
+        expanduser(get_eth_path().unwrap()).unwrap().display()
+    );
+    println!(
+        "✔ {}",
+        expanduser(get_iab_path().unwrap()).unwrap().display()
+    );
+    println!(
+        "✔ {}",
+        expanduser(get_mam_path().unwrap()).unwrap().display()
+    );
+    println!(
+        "✔ {}",
+        expanduser(get_manid_path().unwrap()).unwrap().display()
+    );
+    println!(
+        "✔ {}",
+        expanduser(get_opid_path().unwrap()).unwrap().display()
+    );
+    println!(
+        "✔ {}",
+        expanduser(get_oui_path().unwrap()).unwrap().display()
+    );
+    println!(
+        "✔ {}",
+        expanduser(get_oui36_path().unwrap()).unwrap().display()
+    );
 }
